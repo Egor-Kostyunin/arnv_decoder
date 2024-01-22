ecaf96b75d1dafc4960b738043cbeed30db05d30
Kostyunin Egor <kostyunin.eg@yandex.ru>
2024-01-22 10:11:33 +0300

    Добавленно сообщение при отсутствии аргумента

--- main.c
+++ main.c
@@ -1,7 +1,11 @@
 #include "arnv_decoder.h"
+#include <stdio.h>
 
 int main(int argc,char **argv){
-	if(argc < 2) return 1;
+	if(argc < 2){
+		puts("Использование ./arnv_decoder путь_до_arnv_файла");
+		return 1;
+	};
 	LoadDict();
 	int count = 0;
 	tag* tags_array = ReadARNV(argv[1],&count);
